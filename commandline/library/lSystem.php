3f8650da9c35228aab2224bbc2d5fbed87b4202d
Julius Türich <info@joonlabs.com>
2021-01-10 21:33:10 +0100

    patched last commit

--- commandline/library/lSystem.php
+++ commandline/library/lSystem.php
@@ -4,6 +4,14 @@ abstract class lSystem{
      * returns the current directory
      */
     static function getPWD(){
+        function str_replace_last( $search , $replace , $str ) {
+            if( ( $pos = strrpos( $str , $search ) ) !== false ) {
+                $search_length  = strlen( $search );
+                $str    = substr_replace( $str , $replace , $pos , $search_length );
+            }
+            return $str;
+        }
+
         $pwd = lStorage::get("pwd");
         if($pwd==null) $pwd = str_replace_last("/commandline", "", getcwd());
         lStorage::save("pwd", $pwd);
--- commandline/programms/pwd/init.php
+++ commandline/programms/pwd/init.php
@@ -4,12 +4,4 @@
         $pwd = lSystem::getPWD();
         lCommand::write("$pwd");
     }
-
-    function str_replace_last( $search , $replace , $str ) {
-        if( ( $pos = strrpos( $str , $search ) ) !== false ) {
-            $search_length  = strlen( $search );
-            $str    = substr_replace( $str , $replace , $pos , $search_length );
-        }
-        return $str;
-    }
 ?>
